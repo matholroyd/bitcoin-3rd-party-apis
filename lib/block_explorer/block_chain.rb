327c57f831293dbb0dd220d5c015e19d03572931
Mike Mondragon <mikemondragon@gmail.com>
2013-11-15 09:11:42 -0800

    Support blockexplorer's addressbalance.

--- lib/block_explorer/block_chain.rb
+++ lib/block_explorer/block_chain.rb
@@ -1,46 +1,55 @@
 class BlockExplorer
+
   module BlockChain
 
     def mytransactions(addresses)
       if addresses.is_a?(String)
         addresses = [addresses]
       end
-      
+
       get_json "#{mytransactions_url}/#{addresses.join('.')}"
     end
 
     def getreceivedbyaddress(address, minconf = 0)
-      if minconf > 0 
+      if minconf > 0
         url = "#{getreceivedbyaddress_url}/#{address}/#{minconf}"
       else
         url = "#{getreceivedbyaddress_url}/#{address}"
       end
-      
+
       BigDecimal(open(url).read)
+    end
+
+    def addressbalance(address)
+      BigDecimal(open("#{addressbalance_url}/#{address}").read)
     end
 
     # def rawtx(hash)
     #   get_json "#{RawTx}/#{hash}"
     # end
-    
-    private 
-        
+
+    private
+
+    def addressbalance_url
+      "#{domain}/q/addressbalance"
+    end
+
     def mytransactions_url
       "#{domain}/q/mytransactions"
     end
-    
+
     def rawtx_url
       "#{domain}/rawtx"
     end
-    
+
     def getreceivedbyaddress_url
       "#{domain}/q/getreceivedbyaddress"
     end
-    
+
     def get_json(url)
       JSON.parse open(url).read
     end
   end
-  
+
   extend BlockChain
 end
--- spec/block_explorer/block_explorer_spec.rb
+++ spec/block_explorer/block_explorer_spec.rb
@@ -3,37 +3,38 @@ require 'spec_helper'
 describe BlockExplorer do
   let(:addressA) { '1JDfUiJHZ6pDY6wWYTx86RYjDCW7QxCofs' }
   let(:addressB) { '1KxepETPK2Xz8e5FaZE62y7Rww4LWuJUhG' }
-    
+  let(:addressB) { '1FfmbHfnpaZjKFvyi1okTjJJusN455paPH' }
+
   context '#mytransactions' do
-    
+
     context 'getting transactions for one address' do
       use_vcr_cassette
 
       let(:transactions) { BlockExplorer.mytransactions(addressA) }
-      
+
       it "returns 2 transactions" do
         transactions.count.should == 2
       end
-          
+
       it "wraps the transactions in JSON" do
         transactions.first[0].should == "6c5067ee83752022110fafefefcbbba9f006c2c90bce0d7cba4c28258831e0af"
         transactions.first[1]['out'][0]['value'].should == "0.16200000"
         transactions.first[1]['out'][1]['value'].should == "0.10000000"
       end
     end
-    
+
     context 'getting transactions for 2 addresses' do
       use_vcr_cassette
 
       let(:transactions) { BlockExplorer.mytransactions([addressA, addressB]) }
-    
+
       it "returns 2 transactions" do
         transactions.count.should == BlockExplorer.mytransactions(addressA).count + BlockExplorer.mytransactions(addressB).count
       end
     end
-    
+
   end
-  
+
   context '#getreceivedbyaddress' do
     context "without minconf" do
       use_vcr_cassette
@@ -42,7 +43,7 @@ describe BlockExplorer do
         BlockExplorer.getreceivedbyaddress(addressA).should == BigDecimal("0.1")
       end
     end
-    
+
     context "minconf" do
       use_vcr_cassette
 
@@ -50,6 +51,16 @@ describe BlockExplorer do
         BlockExplorer.getreceivedbyaddress(addressA, 99999999).should == BigDecimal("0")
       end
     end
-    
+
+  end
+
+  context '#addressbalance' do
+
+    use_vcr_cassette
+
+    it "returns the coin balance of the address" do
+      BlockExplorer.addressbalance(addressB).should == BigDecimal("144336.40733762")
+    end
+
   end
 end
